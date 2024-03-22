3d35252c363f7f8c73124f86d6b1213ac8845c10
Robbe <robbevaes95@gmail.com>
2024-03-22 23:30:54 +0100

    Auth

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,19 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import WisemenEslintConfig from '@wisemen/eslint-config-vue'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default [
   ...(await WisemenEslintConfig),
+  {
+    settings: {
+      tailwindcss: {
+        config: path.join(__dirname, './packages/nuxt/tailwind.config.ts'),
+      },
+    },
+  },
   {
     rules: {
       'vue/no-undef-components': 'off',
